388da4e32ae57b84b5287106ef97c60a4370b872
eotkds <eotkds@gmail.com>
2025-03-28 09:54:34 +0900

    By 컴포넌트 재사용하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Post from './components/Post'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  {/* 
+    JSX expressions must have one parent element. 
+    jsx, tsx 여러 요소를 사용할 경우 반드시 부모 요소로 감싸야 한다.
+    */}
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      </>
   )
 }
 
---
+++ src/components/Post.jsx
@@ -0,0 +1,11 @@
+const names = ["Maximilian", "Parker"];
+
+export default function Post() {
+  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+  return (
+    <div>
+        <p>{chosenName}</p>
+        <p>React.js is awsome</p>
+    </div>
+  );
+}

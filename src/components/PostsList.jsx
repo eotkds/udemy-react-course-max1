79ef6ddae8085c4f96cb1393cd5bde83c1384d17
eotkds <eotkds@gmail.com>
2025-03-31 00:27:57 +0900

    By 특별한 children 프로퍼티

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,32 @@
+import { useState } from 'react';
 import Post from './Post';
-import classes from './PostsList.module.css';
 import NewPost from './NewPost';
+import Modal from './Modal';
+import classes from './PostsList.module.css';
+
 
 export default function PostsList() {
+
+    /*
+        250330  왜 NewPost 안에서 useState를 선언하지 않았을까?
+        아마도 posts list 데이터를 보내주기 위해서 일수도, 실제 프로젝트라면 그렇지 않았을 수도 있다.
+    */
+    const [enteredBody, setEnteredBody] = useState('');
+    const [enteredAuthor, setEnteredAuthor] = useState('');
+    function changeBodyHandler(event) {
+      setEnteredBody(event.target.value);
+    }
+
+    function changeAuthorHandler(event) {
+        setEnteredAuthor(event.target.value);
+      }
     return (
         <>
-            <NewPost />
+            <Modal>
+                <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler} />
+            </Modal>
             <ul className={classes.posts}>
-                <Post author="Parker" body="Hello" />
+                <Post author={enteredAuthor} body={enteredBody} />
                 <Post author="Max" body="Learning React is fun!" />
             </ul>
         </>

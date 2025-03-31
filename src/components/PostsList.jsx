2100d9756e8eeeae7bdd3919ed0f8859b51df388
eotkds <eotkds@gmail.com>
2025-03-31 14:46:51 +0900

    By 양식 제출 처리

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+
 import Post from './Post';
 import NewPost from './NewPost';
 import Modal from './Modal';
@@ -11,26 +11,15 @@ export default function PostsList({isPosting, onStopPosting}) {
         250330  왜 NewPost 안에서 useState를 선언하지 않았을까?
         아마도 posts list 데이터를 보내주기 위해서 일수도, 실제 프로젝트라면 그렇지 않았을 수도 있다.
     */
-    const [enteredBody, setEnteredBody] = useState('');
-    const [enteredAuthor, setEnteredAuthor] = useState('');
 
-
-    function changeBodyHandler(event) {
-      setEnteredBody(event.target.value);
-    }
-
-    function changeAuthorHandler(event) {
-        setEnteredAuthor(event.target.value);
-      }
     return (
         <>
             {isPosting && (
             <Modal onClose={onStopPosting}>
-                <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler} />
+                <NewPost onCancel={onStopPosting} />
             </Modal>
             )}
             <ul className={classes.posts}>
-                <Post author={enteredAuthor} body={enteredBody} />
                 <Post author="Max" body="Learning React is fun!" />
             </ul>
         </>

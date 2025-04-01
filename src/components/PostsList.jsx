7d48512d9f6e0d93223fc515b498ab7334e87f41
eotkds <eotkds@gmail.com>
2025-04-01 23:06:43 +0900

    By 로딩 상태 관리

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -7,12 +7,15 @@ import classes from './PostsList.module.css';
 
 export default function PostsList({isPosting, onStopPosting}) {
     const [posts, setPosts] = useState([]);
+    const [isFetching, setIsFetching] = useState(false);
 
     useEffect(() => {
         async function fetchPosts() {
+            setIsFetching(true);
             const response = await fetch('http://localhost:8080/posts');
             const data = await response.json();
             setPosts(data.posts);
+            setIsFetching(false);
         }
         fetchPosts();
     }, []);
@@ -45,15 +48,20 @@ export default function PostsList({isPosting, onStopPosting}) {
                 <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
             </Modal>
             )}
-            {posts.length > 0 && (
+            {!isFetching && posts.length > 0 && (
                 <ul className={classes.posts}>
                     {posts.map((post, idx)=> <Post key={idx} author={post.author} body={post.body} />)}
                 </ul>
             )}
-            {posts.length === 0 && (
+            {!isFetching &&posts.length === 0 && (
                 <div style={{textAlign: 'center', color: 'white'}}>
                     <h2>There are no posts yet.</h2>
                     <p>Start adding some!</p>
+                </div>
+            )}
+            {isFetching && (
+                <div style={{textAlign: 'center', color: 'white'}}>
+                    <h2>Loading Posts...</h2>
                 </div>
             )}
         </>

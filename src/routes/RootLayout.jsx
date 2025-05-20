b5342f2d3847dcf044566910934557c27c61dcb8
eotkds <eotkds@gmail.com>
2025-05-20 13:44:41 +0900

    pull request 후 브랜치 테스트2

--- src/routes/RootLayout.jsx
+++ src/routes/RootLayout.jsx
@@ -5,6 +5,7 @@ export default function RootLayout() {
     return (
         <>
             <h3>RootLayout</h3>
+            <h2>test2</h2>
             <MainHeader />
             <Outlet />
         </>
